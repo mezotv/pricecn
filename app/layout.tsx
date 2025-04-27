910605fe3fe69e949603b179fddde9e2036aa95f
John Yeo <johnyeocx@gmail.com>
2025-04-27 17:44:51 +0100

    added theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { RootProvider } from "fumadocs-ui/provider";
-// import "fumadocs-ui/css/neutral.css";
-// import "fumadocs-ui/css/preset.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <RootProvider>{children}</RootProvider>
+        {children}
       </body>
     </html>
   );

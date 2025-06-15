5a8cd8cd34e77c82423ce5c26dd9bdd55b139afc
Dominik Koch <dominik@koch-bautechnik.de>
2025-06-15 22:25:29 +0200

    fix: tiny cleanup and responsiveness

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -3,5 +3,11 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout {...baseOptions}>
+      <div className="pt-8 md:pt-16 bg-background dark:bg-black">
+        {children}
+      </div>
+    </HomeLayout>
+  );
 }
